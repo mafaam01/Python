2256f2c51696b48781ca8c7be156443220dda37c
mafaam01 <94179673+mafaam01@users.noreply.github.com>
2022-02-19 15:15:59 -0300

    Add files via upload

---
+++ Aula  - 1/Aula - 1.py
@@ -0,0 +1,24 @@
+
+"""
+# Comentário ""."
+print('Linha 1')
+print('Linha 2')
+# Falando sobre do que se trata essa linha
+print('Linha 3')
+print('Linha 4')
+print('Linha 5')
+"""
+print('Python.py')
+
+"""
+Algum texto
+"""
+
+'''
+Algum texto
+'''
+
+# Explicando
+print('123456')
+
+print('Sempre tem que colocar dois espaços para não dar erro com o python')  # Outro comentário
